467aace5c25ea7818e7b7ad6a1426534c176f2e5
Sebastián Ramírez Magrí <sebasmagri@gmail.com>
2012-09-23 07:42:46 -0430

    Add the setup.py script. Updates/Fixes to the README

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
  Mezzanine Polls
 =================
 
-Mezzanine Polls is a simple Poll application built for the _`Django` based CMS _`Mezzanine`.
+Mezzanine Polls is a simple Poll application built for the `Django`_ based CMS `Mezzanine`_.
 
 Features
 ========
@@ -14,7 +14,7 @@ Features
 Installation and Usage
 ======================
 
-Install the app by using _`pip`::
+Install the app by using `pip`_::
 
   $ pip install mezzanine_polls
 
@@ -25,7 +25,7 @@ Go to the admin site and create new polls in the ``Pages`` section
 Development
 ===========
 
-New features and fixes are welcome. Better if it comes with a patch or pull request. You can find the latest development sources at _`Bitbucket` or _`GitHub`. Issues are only tracked at the Bitbucket project page.
+New features and fixes are welcome. Better if it comes with a patch or pull request. You can find the latest development sources at `Bitbucket`_ or `GitHub`_. Issues are only tracked at the Bitbucket project page.
 
 .. _`Mezzanine`: http://mezzanine.jupo.org/
 .. _`Django`: http://djangoproject.com/
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+import mezzanine_polls
+
+setup(
+    name='Mezzanine Polls',
+    version=mezzanine_polls.__version__,
+    url='https://bitbucket.org/sebasmagri/mezzanine_polls',
+    author='Sebastian Magri',
+    author_email='sebasmagri@gmail.com',
+    license='BSD',
+    description='Polls application for the Mezzanine CMS',
+    long_description=open('README.rst').read(),
+    keywords='django, mezzanine, polls',
+    packages=find_packages(),
+    setup_requires=('setuptools'),
+    install_requires=('setuptools', 'mezzanine'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',],
+    zip_safe=False,
+    include_package_data=True,
+)
